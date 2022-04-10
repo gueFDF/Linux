d9cda88b670330b4a5c2672219f7054e4813e4ba
郭天宇 <3237455241@qq.com>
2022-04-10 23:10:01 +0800

    myshell

---
+++ myshell/myshell.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+//定义颜色
+/*#define GREEN "\e[1;32m"
+#define BLUE "\e[1;34m"
+#define RED "\e[1;31m"
+#define WHITE "\e[0m"*/
+#define MAX 128
+int main()
+{
+    char commod[MAX];
+    while(1)
+    {
+        //打印提示符
+        printf("gty@gty-Lenovo-Legion:~/myshell/c$ ");
+        fflush(stdout);//清空缓冲区,默认为行缓冲，提示符不是以\n结尾的
+        while(1)
+        {
+
+        }
+    }
+}
