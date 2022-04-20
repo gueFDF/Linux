b89da96f2e0e648f5c0b70c7ab6cfc7d1a73052c
郭天宇 <3237455241@qq.com>
2022-04-21 00:23:06 +0800

    sigaction函数实现SIGINT信号1捕捉

---
+++ Linux学习/进程学习/sigaction.c
@@ -0,0 +1,23 @@
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+void sig_catch(int signo)
+{
+    printf("catch you ! %d\n",signo);
+} 
+int main()
+{
+    struct sigaction act,oldact;
+    act.sa_handler = sig_catch;//set callback function name
+    sigemptyset(&act.sa_mask);//清零set mask when sig_catch working
+    act.sa_flags=0;            //usually use
+
+    int ret=sigaction(SIGINT,&act,&oldact);
+    if(ret==-1)
+    {
+        perror("error");
+        exit(1);
+    }
+    while(1);
+    return 0;
+}
