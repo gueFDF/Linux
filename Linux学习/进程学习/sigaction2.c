53a915b09f57bd02da652ddec46dd34fc6290298
郭天宇 <3237455241@qq.com>
2022-04-21 00:57:22 +0800

    捕捉信号特性测试

---
+++ Linux学习/进程学习/sigaction2.c
@@ -0,0 +1,26 @@
+#include<signal.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+void sig_catch(int signo)
+{
+    printf("catch you ! %d\n",signo);
+    sleep(10);
+
+} 
+int main()
+{
+    struct sigaction act,oldact;
+    act.sa_handler = sig_catch;//set callback function name
+    sigemptyset(&act.sa_mask);//清零set mask when sig_catch working
+    act.sa_flags=0;            //usually use
+    sigaddset(&act.sa_mask,SIGQUIT);//阻塞SIGQUIT信号
+    int ret=sigaction(SIGINT,&act,&oldact);
+    if(ret==-1)
+    {
+        perror("error");
+        exit(1);
+    }
+    while(1);
+    return 0;
+}
