47eeeec0c42ff0f89f2e95ffffe0b025828fb27c
郭天宇 <3237455241@qq.com>
2022-04-24 23:16:42 +0800

    线程创建

---
+++ Linux学习/线程学习/pthread_create.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<errno.h>
+#include<unistd.h>
+//#include<sleep.h>
+void* tfn(void*arg)
+{
+    printf("phread:pid = %d,tid = %lu\n",getpid(),pthread_self());
+}
+int main()
+{
+    pthread_t tid;
+    printf("mian : pid = %d ,tid = %lu\n",getpid(),pthread_self());
+    int ret=pthread_create(&tid,NULL,tfn,NULL);
+    if(ret!=0)
+    {
+        perror("pthread");
+    }
+    sleep(1);
+    return 0;
+}
