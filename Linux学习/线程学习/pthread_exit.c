a316d83bd8a1c00cbae41e2b3635a3f755230d1e
郭天宇 <3237455241@qq.com>
2022-04-25 00:56:50 +0800

    退出当前线程(可以是主线程),不影响其他线程,pthread_exit

---
+++ Linux学习/线程学习/pthread_exit.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+void func()
+{
+      pthread_exit(NULL);
+}
+void*tfn(void*argv)
+{
+    int i=(int)argv;
+    sleep(i);
+    if(i==2)//第三个线程退出
+    {
+       // exit(1);//表示退出进程
+       //return NULL;//表示返回到函数调用者那里
+       func();
+      
+    }
+    printf("I am %d ,pthread: pid = % d,tid = %lu\n",i+1,getpid(),pthread_self());
+}
+int main()
+{
+    int i=0;
+    int ret;
+    pthread_t tid;
+    for(i=0;i<5;i++)
+    {
+        ret=pthread_create(&tid,NULL,tfn,(void*)i);
+        if(ret!=0)
+        {
+            perror("pthread_create");
+            exit(1);
+        }
+    }
+    //sleep(i);
+    printf("i am main\n");
+    //return 0;
+    pthread_exit(NULL);
+}
